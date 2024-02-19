f33a9ad3ef3b31b5c347e92f543e0e6064c790e3
churchrat <mihailkrustev08@gmail.com>
2024-02-19 14:05:24 +0200

    Lists1

---
+++ ListCreate/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ListCreate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedete spisuk ot cvetove>>>>Done za krai<<<<:");
+            List<string> ColorsList = new List<string>();
+            string input = null;
+            do
+            {
+                input = Console.ReadLine();
+                ColorsList.Add(input);
+            } while (input != "done");
+            ColorsList.Remove(input);
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            foreach (string kvadrat in ColorsList)
+            {
+                Console.WriteLine(kvadrat);
+            }
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            Console.WriteLine("0 do " + (ColorsList.Count-1));
+            int index = int.Parse(Console.ReadLine());
+            ColorsList.RemoveAt(index);
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            //for (int i = 0; i < ColorsList.Count; i++) 
+            //{
+            //    Console.WriteLine(ColorsList[i]);
+            //}
+            foreach (string kvadrat in ColorsList)
+            {
+                Console.WriteLine(kvadrat);
+            }
+        }
+    }
+}
